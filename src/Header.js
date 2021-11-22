c7aacb06541a8d14ae5769db1b6bf24e7bbed388
Shakim Harris <amenraprod@gmail.com>
2021-11-21 23:12:51 -0500

    imported icons

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import MicIcon from '@mui/icons-material/Mic';
+import ViewModuleIcon from '@mui/icons-material/ViewModule';
+import FeaturedPlayListIcon from '@mui/icons-material/FeaturedPlayList';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function Header () {
     return (
         <div className="header">
             <div className="header__logo">
+            <MenuIcon />
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/2560px-Logo_of_YouTube_%282015-2017%29.svg.png" className="App-logo" alt="logo" />
                <h4>This is the header </h4>
                 <input type="text" />
-                 <AccessAlarmIcon />
+                <SearchIcon />
+                <MicIcon />
+                <ViewModuleIcon />
+                <FeaturedPlayListIcon />
+                <AccountCircleIcon />
                 
             </div>
 
