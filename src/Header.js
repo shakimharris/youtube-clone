162a79e82658710ef0fd117e7d59950b59436a39
Shakim Harris <amenraprod@gmail.com>
2021-11-22 00:35:03 -0500

    styled header with flexbox

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import './Header.css';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
-import MicIcon from '@mui/icons-material/Mic';
 import ViewModuleIcon from '@mui/icons-material/ViewModule';
 import FeaturedPlayListIcon from '@mui/icons-material/FeaturedPlayList';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
@@ -9,20 +9,21 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 function Header () {
     return (
         <div className="header">
-            <div className="header__logo">
+            <div className="header-left">
             <MenuIcon />
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/2560px-Logo_of_YouTube_%282015-2017%29.svg.png" className="App-logo" alt="logo" />
-               <h4>This is the header </h4>
-                <input type="text" />
-                <SearchIcon />
-                <MicIcon />
-                <ViewModuleIcon />
-                <FeaturedPlayListIcon />
-                <AccountCircleIcon />
-                
+             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/2560px-Logo_of_YouTube_%282015-2017%29.svg.png" className="logo" />
+             </div>
+            <div className="header-center">
+            <input placeholder="Search" type="text" />
+            <SearchIcon className="search_icon" />
+                </div>
+                <div className="header-right">
+                    <ViewModuleIcon className="header-icon" />
+                    <FeaturedPlayListIcon className="header-icon" />
+                    <AccountCircleIcon className="header-icon" />
+                    </div>
             </div>
 
-        </div>
     )
 }
 
