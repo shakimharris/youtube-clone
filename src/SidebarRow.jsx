5560238319db100d066e7e087ccba8142105dcdd
Shakim Harris <amenraprod@gmail.com>
2021-11-22 01:46:41 -0500

    chng icons

--- src/SidebarRow.jsx
+++ src/SidebarRow.jsx
@@ -3,9 +3,9 @@ import './SidebarRow.css';
 
 function SidebarRow({ Icon, title }){
  return (
-    <div className="sidebar-row">
-        <Icon />
-        <h1>{title}</h1>
+    <div className="sidebar__row">
+        <Icon className="sidebar__icon" />
+        <h2 className="sidebar__title">{title}</h2>
 
     </div>
 )
