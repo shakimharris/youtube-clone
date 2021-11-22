29a371d547e480bdd4d987fd7e762db89f751c39
Shakim Harris <amenraprod@gmail.com>
2021-11-22 04:07:40 -0500

    added routing

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Recommended from './Recommended';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+    <Router>
       <Header />
-      <div className="app__page">
-       <Sidebar />
-       <Recommended />
-       </div>
-    </div>
+       <Switch>
+         <Route path="/">
+         <div className="app__page">
+          <Sidebar />
+           <Recommended />
+           </div>
+          </Route>
+         <Route path="/search">
+           <h1>This is the search page.</h1>
+           </Route>
+           </Switch>
+           </Router>
+           </div>
   );
 }
 
