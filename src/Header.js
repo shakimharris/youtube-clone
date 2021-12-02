1b89a1ee394f5381d81d2a3213d664f9f67e7eba
Shakim Harris <amenraprod@gmail.com>
2021-12-02 09:06:48 -0500

    added state and ability to search

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
@@ -7,22 +8,26 @@ import FeaturedPlayListIcon from '@mui/icons-material/FeaturedPlayList';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function Header () {
+   const [searchInput, setSearchInput] = useState("");
+
     return (
         <div className="header">
             <div className="header-left">
-            <MenuIcon />
-             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/2560px-Logo_of_YouTube_%282015-2017%29.svg.png" className="logo" />
-             </div>
-            <div className="header-center">
-            <input placeholder="Search" type="text" />
-            <SearchIcon className="search_icon" />
-                </div>
-                <div className="header-right">
-                    <ViewModuleIcon className="header-icon" />
-                    <FeaturedPlayListIcon className="header-icon" />
+             <MenuIcon />
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/2560px-Logo_of_YouTube_%282015-2017%29.svg.png" className="logo" />
+              </div>
+                <div className="header-center">
+                 <input value={searchInput} onChange={e => setSearchInput(e.target.value)} placeholder="Search" type="text" />
+                  <Link to={`/search/${searchInput}`}>
+                    <SearchIcon className="search_icon" />
+                      </Link>
+                       </div>
+                        <div className="header-right">
+                        <ViewModuleIcon className="header-icon" />
+                       <FeaturedPlayListIcon className="header-icon" />
                     <AccountCircleIcon className="header-icon" />
-                    </div>
-            </div>
+                   </div>
+                  </div>
 
     )
 }
